6d3224d902a2669849dedf796e979b525f22066c
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2021-08-12 18:38:43 +0600

    Built enemy AI 1

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -12,34 +12,88 @@ public class Enemy : MonoBehaviour
     //Patrolling
     public Vector3 walkPoint;
     bool walkPointSet;
-    public float walkPointRange;
 
     //Attacking
+    [SerializeField]private float attackRange;
     public float timeBetweenAttacks;
     bool alreadyAttacked;
 
-    //states
-    public float sightRange, attackRange;
-    public bool playerInSightRange, playerInAttackRange;
 
+    //pathfinding
     [SerializeField]private GameObject[] walkPoints;
     [SerializeField]private int wpIndex;
+
+    //FOV
+    public float radius;
+    [Range(0,360)]
+    public float angle;
+
+    public LayerMask obstructionMask;
+
+    public bool canSeePlayer;
 
     private void Awake() {
         player = GameObject.Find("Player").transform;
         agent = GetComponent<NavMeshAgent>();
+        StartCoroutine(FOVRoutine());
+    }
+
+
+    private IEnumerator FOVRoutine()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.2f);
+
+        while (true)
+        {
+            yield return wait;
+            FieldOfViewCheck();
+        }
+    }
+
+    private void FieldOfViewCheck()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, whatIsPlayer);
+        //checks if player in fov
+        if (rangeChecks.Length != 0)
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            //check if in angle
+            if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                //check if wall between enemy and player
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
+                    //no obstacles
+                    canSeePlayer = true;
+                else
+                    canSeePlayer = false;
+            }
+            else
+                canSeePlayer = false;
+        }
+        else if (canSeePlayer)
+            canSeePlayer = false;
     }
 
     private void Update() {
         //check sight and attack range
-        playerInSightRange = Physics.CheckSphere(transform.position,sightRange,whatIsPlayer);
+        /* playerInSightRange = Physics.CheckSphere(transform.position,sightRange,whatIsPlayer);
         playerInAttackRange = Physics.CheckSphere(transform.position,attackRange,whatIsPlayer);
 
         if (!playerInSightRange && !playerInAttackRange) Patrolling();
         if (playerInSightRange && !playerInAttackRange) ChasePlayer();
-        if (playerInSightRange && playerInAttackRange) AttackPlayer();
+        if (playerInSightRange && playerInAttackRange) AttackPlayer(); */
+
+        if ( canSeePlayer ) {
+            ChasePlayer();
+        } else Patrolling();
         
     }
+
+    
 
     private void Patrolling() {
         if ( !walkPointSet ) SearchWalkPoint();
@@ -82,6 +136,11 @@ public class Enemy : MonoBehaviour
 
     private void ChasePlayer() {
         agent.SetDestination(player.position);
+        float playerEnemyDist = Vector3.Magnitude(player.position - transform.position);
+        if ( playerEnemyDist <= attackRange )
+        {
+            AttackPlayer();
+        }
     }
 
     private void AttackPlayer() {
@@ -103,11 +162,15 @@ public class Enemy : MonoBehaviour
         alreadyAttacked = false;
     }
 
-    private void OnDrawGizmosSelected() {
+    /* private void OnDrawGizmosSelected() {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, attackRange);
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, sightRange);
-    }
+    } */
+
+    
+
+
 
 }
---
+++ Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float radius;
+    [Range(0,360)]
+    public float angle;
+
+    public GameObject playerRef;
+
+    public LayerMask targetMask;
+    public LayerMask obstructionMask;
+
+    public bool canSeePlayer;
+
+    private void Start()
+    {
+        playerRef = GameObject.FindGameObjectWithTag("Player");
+        StartCoroutine(FOVRoutine());
+    }
+
+    private IEnumerator FOVRoutine()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.2f);
+
+        while (true)
+        {
+            yield return wait;
+            FieldOfViewCheck();
+        }
+    }
+
+    private void FieldOfViewCheck()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
+        //checks if player in fov
+        if (rangeChecks.Length != 0)
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            //check if in angle
+            if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                //check if wall between enemy and player
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
+                    //no obstacles
+                    canSeePlayer = true;
+                else
+                    canSeePlayer = false;
+            }
+            else
+                canSeePlayer = false;
+        }
+        else if (canSeePlayer)
+            canSeePlayer = false;
+    }
+}
